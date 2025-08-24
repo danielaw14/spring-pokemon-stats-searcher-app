52cacf526c44399c55d020757b7c697967c0cead
Daniel Ward <wardda3@appstate.edu>
2025-08-24 16:58:28 -0400

    Initial Spring Boot Pokemon Stat Searcher App (Web + Validation)

---
+++ src/main/java/com/example/pokemonstatssearcher/SpringPokemonStatsSearcherAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokemonstatssearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPokemonStatsSearcherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPokemonStatsSearcherAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pokemonstatssearcher/SpringPokemonStatsSearcherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemonstatssearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPokemonStatsSearcherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
